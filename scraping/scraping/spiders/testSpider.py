ac6fb7b142362adccdec20bc984ebb644d89d491
Mouad Elhaouari <jm_elhaouari@esi.dz>
2023-09-23 15:59:42 +0100

    add all websites to start_urls except medium based blogs

--- scraping/scraping/spiders/testSpider.py
+++ scraping/scraping/spiders/testSpider.py
@@ -1,18 +1,81 @@
 import scrapy
+from scrapy.linkextractors import LinkExtractor
 
 
-class DiscordBlogSpider(scrapy.Spider):
-    name = 'discord_blog'
-    base_url = 'https://discord.com'
-    start_urls = ['https://discord.com/category/engineering']
+class BlogRules:
+    def __init__(self, domain, allow, deny, restrict_css=None):
+        self.domain = domain
+        self.allow = allow
+        self.deny = deny
+        self.restrict_css = restrict_css
+
+
+class BlogSpider(scrapy.Spider):
+    name = 'engblog'
+    start_urls = ['https://discord.com/category/engineering',
+                  'https://blog.cloudflare.com/tag/developers/',
+                  'https://shopify.engineering/',
+                  'https://stripe.com/blog/engineering',
+                  'https://github.blog/category/engineering/',
+                  'https://www.uber.com/en-ES/blog/engineering/',
+                  'https://engineering.linkedin.com/blog',
+                  'https://engineering.fb.com/',
+                  'https://engineering.atspotify.com/'
+                  ]
+    blogs = {
+        'https://discord.com/category/engineering': BlogRules(
+            domain="discord.com",
+            allow=["/blog/"],
+            deny=[]
+        ),
+        'https://blog.cloudflare.com/tag/developers/': BlogRules(
+            domain="blog.cloudflare.com",
+            allow=[],
+            deny=['/tag/', '/search/', '/author/',
+                  '^https://blog.cloudflare.com/$', 'https://blog.cloudflare.com/([a-z]{2}-[a-z]{2})']),
+        'https://shopify.engineering/': BlogRules(
+            domain="shopify.engineering", allow=[],
+            deny=["^https://shopify.engineering/$", "^https://shopify.engineering/work-with-us$",
+                  "^https://shopify.engineering/sponsorship$",
+                  "/topics/", "/search?", "/?page", "/#"],
+            restrict_css=['.grid__item--desktop-up-two-thirds']),
+        'https://stripe.com/blog/engineering': BlogRules(
+            domain="stripe.com",
+            allow=['/blog/'],
+            deny=['/blog/engineering', '/blog/feed.rss']
+        ),
+        'https://github.blog/category/engineering/': BlogRules(
+            domain='github.blog',
+            allow=['https://github.blog/\d{4}-\d{2}-\d{2}'],
+            deny=[]
+        ),
+        'https://www.uber.com/en-ES/blog/engineering/': BlogRules(
+            domain='www.uber.com',
+            allow=['/en-ES/blog/'],
+            deny=['/blog/engineering/', '/blog/ride/', '/blog/business/',
+                  '/blog/careers/', '^https://www.uber.com/en-ES/blog/$']
+        ),
+        'https://engineering.linkedin.com/blog': BlogRules(
+            domain='engineering.linkedin.com',
+            allow=['/blog/\d{4}/'],
+            deny=[]
+        ),
+        'https://engineering.fb.com/': BlogRules(
+            domain='engineering.fb.com',
+            allow=['/\d{4}/\d{2}/\d{2}'],
+            deny=[]
+        ),
+        'https://engineering.atspotify.com/': BlogRules(
+            domain='engineering.atspotify.com',
+            allow=['/\d{4}/\d{2}'],
+            deny=[]
+        )
+    }
 
     def parse(self, response):
-        # Extract all links from the page
-        links = response.css('a::attr(href)').extract()
-
-        # Filter links that start with 'https://discord.com/blog/'
-        filtered_links = [link for link in links if link.startswith('/blog/')]
-
-        # Print the filtered links
-        for link in filtered_links:
-            self.logger.info(self.base_url + link)
+        rules = self.blogs[response.url]
+        link_extractor = LinkExtractor(
+            allow=rules.allow, deny=rules.deny, allow_domains=rules.domain, restrict_css=rules.restrict_css)
+        links = link_extractor.extract_links(response)
+        for link in links:
+            print(link.url)
