a22959f10a580b3b1db7499b832f40e3db955b07
Mouad Elhaouari <jm_elhaouari@esi.dz>
2023-09-29 23:06:10 +0100

    cover all blogs date and title patterns

--- scraping/scraping/spiders/testSpider.py
+++ scraping/scraping/spiders/testSpider.py
@@ -1,96 +1,36 @@
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scraping.items import BlogArticle
-
-
-class BlogRules:
-    def __init__(self, domain, allow, deny, restrict_css=None):
-        self.domain = domain
-        self.allow = allow
-        self.deny = deny
-        self.restrict_css = restrict_css
+from scraping.utils import blogs
 
 
 class BlogSpider(scrapy.Spider):
     name = 'engblog'
-    start_urls = [  # 'https://discord.com/category/engineering',
-        #   'https://blog.cloudflare.com/tag/developers/',
-        #   'https://shopify.engineering/',
-        'https://stripe.com/blog/engineering',
-        #   'https://github.blog/category/engineering/',
-        #   'https://www.uber.com/en-ES/blog/engineering/',
-        #   'https://engineering.linkedin.com/blog',
-        #   'https://engineering.fb.com/',
-        #   'https://engineering.atspotify.com/'
-    ]
-    blogs = {
-        'https://discord.com/category/engineering': BlogRules(
-            domain="discord.com",
-            allow=["/blog/"],
-            deny=[]
-        ),
-        'https://blog.cloudflare.com/tag/developers/': BlogRules(
-            domain="blog.cloudflare.com",
-            allow=[],
-            deny=['/tag/', '/search/', '/author/',
-                  '^https://blog.cloudflare.com/$', 'https://blog.cloudflare.com/([a-z]{2}-[a-z]{2})']),
-        'https://shopify.engineering/': BlogRules(
-            domain="shopify.engineering", allow=[],
-            deny=["^https://shopify.engineering/$", "^https://shopify.engineering/work-with-us$",
-                  "^https://shopify.engineering/sponsorship$",
-                  "/topics/", "/search?", "/?page", "/#"],
-            restrict_css=['.grid__item--desktop-up-two-thirds']),
-        'https://stripe.com/blog/engineering': BlogRules(
-            domain="stripe.com",
-            allow=['/blog/'],
-            deny=['/blog/engineering', '/blog/feed.rss']
-        ),
-        'https://github.blog/category/engineering/': BlogRules(
-            domain='github.blog',
-            allow=['https://github.blog/\d{4}-\d{2}-\d{2}'],
-            deny=[]
-        ),
-        'https://www.uber.com/en-ES/blog/engineering/': BlogRules(
-            domain='www.uber.com',
-            allow=['/en-ES/blog/'],
-            deny=['/blog/engineering/', '/blog/ride/', '/blog/business/',
-                  '/blog/careers/', '^https://www.uber.com/en-ES/blog/$']
-        ),
-        'https://engineering.linkedin.com/blog': BlogRules(
-            domain='engineering.linkedin.com',
-            allow=['/blog/\d{4}/'],
-            deny=[]
-        ),
-        'https://engineering.fb.com/': BlogRules(
-            domain='engineering.fb.com',
-            allow=['/\d{4}/\d{2}/\d{2}'],
-            deny=[]
-        ),
-        'https://engineering.atspotify.com/': BlogRules(
-            domain='engineering.atspotify.com',
-            allow=['/\d{4}/\d{2}'],
-            deny=[]
-        )
-    }
+    BLOGS = blogs
+    start_urls = list(BLOGS.keys())
 
     def parse(self, response):
-        rules = self.blogs[response.url]
+        blog = self.BLOGS[response.url]
         link_extractor = LinkExtractor(
-            allow=rules.allow, deny=rules.deny, allow_domains=rules.domain, restrict_css=rules.restrict_css)
+            allow=blog.get('allow', None), deny=blog.get('deny', None),
+            allow_domains=blog.get('domain', None), restrict_css=blog.get('restrict_css', None))
         links = link_extractor.extract_links(response)
         for link in links:
-            yield scrapy.Request(link.url, self.getDetails)
+            yield scrapy.Request(link.url, callback=self.getDetails, cb_kwargs=dict(main_url=response.url))
 
-    def getDetails(self, response):
+    def getDetails(self, response, main_url):
         yield BlogArticle(url=response.url, date=self.getDate(
-            response), title=self.getTitle(response.url))
+            response, main_url), title=self.getTitle(response, main_url))
 
-    def getDate(self, response):
-        return response.xpath('//@datetime').get()
+    def getDate(self, response, main_url):
+        query = self.BLOGS[main_url].get('date', '//@datetime')
+        return response.xpath(query).get()
 
-    def getTitle(self, url):
+    def getTitle(self, response, main_url):
+        url = response.url
         url_parts = url.split('/')
-        title_with_hyphens = url_parts[-1]
+        title_with_hyphens = url_parts[-2] if url.endswith(
+            '/') else url_parts[-1]
         title_words = title_with_hyphens.split('-')
-        title = ' '.join(title_words)
-        return title
+        rawTitle = ' '.join(title_words)
+        return self.BLOGS[main_url]['title'](rawTitle) if 'title' in self.BLOGS[main_url].keys() else rawTitle
---
+++ scraping/scraping/utils.py
@@ -0,0 +1,59 @@
+import re
+
+
+def processGithubBlogTitle(rawTitle):
+    date_pattern = re.compile(r'\b\d{4} \d{2} \d{2}\b')
+    title = re.sub(date_pattern, '', rawTitle).strip()
+    return title
+
+
+blogs = {
+    'https://discord.com/category/engineering': {
+        'domain': ["discord.com"],
+        'allow': ["/blog/"],
+        'date': "//div[@class='blog-post-author-name']/text()"
+        # date neeed formating
+    },
+    'https://blog.cloudflare.com/tag/developers/': {
+        'domain': ['blog.cloudflare.com'],
+        'deny': ['/tag/', '/search/', '/author/', '^https://blog.cloudflare.com/$', 'https://blog.cloudflare.com/([a-z]{2}-[a-z]{2})']
+    },
+    'https://shopify.engineering/': {
+        'domain': ["shopify.engineering"],
+        'deny': ["^https://shopify.engineering/$", "^https://shopify.engineering/work-with-us$", "^https://shopify.engineering/sponsorship$", "/topics/", "/search?", "/?page", "/#"],
+        'restrict_css': ['.grid__item--desktop-up-two-thirds']
+    },
+    'https://stripe.com/blog/engineering': {
+        'domain': ["stripe.com"],
+        'allow': ['/blog/'],
+        'deny': ['/blog/engineering', '/blog/feed.rss']
+    },
+    'https://github.blog/category/engineering/': {
+        'domain': ['github.blog'],
+        'allow': ['https://github.blog/\d{4}-\d{2}-\d{2}'],
+        'title': processGithubBlogTitle
+    },
+    'https://www.uber.com/en-ES/blog/engineering/': {
+        'domain': ['www.uber.com'],
+        'allow': ['/en-ES/blog/'],
+        'deny': ['/blog/engineering/', '/blog/ride/', '/blog/business/', '/blog/careers/', '^https://www.uber.com/en-ES/blog/$'],
+        'date': "//span[@class='b5 gt b7 ff hc']/text()"
+        # date need special formating + year missing
+    },
+    'https://engineering.linkedin.com/blog': {
+        'domain': ['engineering.linkedin.com'],
+        'allow': ['/blog/\d{4}/'],
+        'date': "//div[@class='date']/text()"
+        # date need special formating
+    },
+    'https://engineering.fb.com/': {
+        'domain': ['engineering.fb.com'],
+        'allow': ['/\d{4}/\d{2}/\d{2}'],
+    },
+    'https://engineering.atspotify.com/': {
+        'domain': ['engineering.atspotify.com'],
+        'allow': ['/\d{4}/\d{2}'],
+        'date': "//span[@class='date']/text()"
+        # date need formatting
+    }
+}
