e858165cc2a16bb1cfda37d9d4a375bf8335d429
Mouad Elhaouari <jm_elhaouari@esi.dz>
2024-01-13 11:03:26 +0100

    format code with black & add requirements.txt

--- scraping/scraping/spiders/testSpider.py
+++ scraping/scraping/spiders/testSpider.py
@@ -7,25 +7,36 @@ from datetime import datetime
 
 
 class BlogSpider(scrapy.Spider):
-    name = 'engblog'
+    name = "engblog"
+
     BLOGS = blogs
     start_urls = list(BLOGS.keys())
 
     def parse(self, response):
         blog = self.BLOGS[response.url]
         link_extractor = LinkExtractor(
-            allow=blog.get('allow', None), deny=blog.get('deny', None),
-            allow_domains=blog.get('domain', None), restrict_css=blog.get('restrict_css', None))
+            allow=blog.get("allow", None),
+            deny=blog.get("deny", None),
+            allow_domains=blog.get("domain", None),
+            restrict_css=blog.get("restrict_css", None),
+        )
         links = link_extractor.extract_links(response)
         for link in links:
-            yield scrapy.Request(link.url, callback=self.getDetails, cb_kwargs=dict(main_url=response.url))
+            yield scrapy.Request(
+                link.url,
+                callback=self.getDetails,
+                cb_kwargs=dict(main_url=response.url),
+            )
 
     def getDetails(self, response, main_url):
-        yield BlogArticle(url=response.url, date=self.getDate(
-            response, main_url), title=self.getTitle(response, main_url))
+        yield BlogArticle(
+            url=response.url,
+            date=self.getDate(response, main_url),
+            title=self.getTitle(response, main_url),
+        )
 
     def getDate(self, response, main_url):
-        query = self.BLOGS[main_url].get('date', '//@datetime')
+        query = self.BLOGS[main_url].get("date", "//@datetime")
         rawDate = response.xpath(query).get()
         # default to get the current year when it is not specified in the rawDate
         parsedDateTime = parser.parse(rawDate, default=datetime.now())
@@ -33,9 +44,12 @@ class BlogSpider(scrapy.Spider):
 
     def getTitle(self, response, main_url):
         url = response.url
-        url_parts = url.split('/')
-        title_with_hyphens = url_parts[-2] if url.endswith(
-            '/') else url_parts[-1]
-        title_words = title_with_hyphens.split('-')
-        rawTitle = ' '.join(title_words)
-        return self.BLOGS[main_url]['title'](rawTitle) if 'title' in self.BLOGS[main_url].keys() else rawTitle
+        url_parts = url.split("/")
+        title_with_hyphens = url_parts[-2] if url.endswith("/") else url_parts[-1]
+        title_words = title_with_hyphens.split("-")
+        rawTitle = " ".join(title_words)
+        return (
+            self.BLOGS[main_url]["title"](rawTitle)
+            if "title" in self.BLOGS[main_url].keys()
+            else rawTitle
+        )
