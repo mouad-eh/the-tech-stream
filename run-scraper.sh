c720e5734418536b84c66ec7bc2244efa460c1df
Mouad Elhaouari <jm_elhaouari@esi.dz>
2024-10-24 13:29:33 +0200

    prod1 http setup quick fix

--- run-scraper.sh
+++ run-scraper.sh
@@ -5,7 +5,7 @@ DOCKERFILE_PATH="./scraping"
 ENV_FILE="./db.env" 
 CRON_JOB="0 */12 * * * docker run --rm --env-file $ENV_FILE $IMAGE_NAME"
 
-docker build -t $IMAGE_NAME -f $DOCKERFILE_PATH .
+docker build -t $IMAGE_NAME $DOCKERFILE_PATH
 
 docker run --rm --env-file $ENV_FILE $IMAGE_NAME
 
