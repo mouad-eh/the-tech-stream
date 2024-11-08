0904951f6058840d8a187c24dc4015aa50cbfc45
Mouad Elhaouari <jm_elhaouari@esi.dz>
2024-11-08 10:15:47 +0100

    updates for prod1 and prod2

---
+++ scraping/main-prod.py
@@ -0,0 +1,22 @@
+from scrapy.crawler import CrawlerRunner
+from scrapy.utils.log import configure_logging
+from scrapy.utils.project import get_project_settings
+from scrapy.utils.reactor import install_reactor
+install_reactor('twisted.internet.epollreactor.EPollReactor')
+from twisted.internet import reactor,defer
+from scraping.spiders.blogSpider import BlogSpider
+from scraping.data import BLOGS
+
+
+@defer.inlineCallbacks                       
+def crawl(runner):
+    for blog in BLOGS:
+        yield runner.crawl(BlogSpider, blog_name=blog, **BLOGS[blog])
+    reactor.stop()
+
+def scraper(event, context):
+    configure_logging(get_project_settings())
+    runner = CrawlerRunner(get_project_settings())
+    crawl(runner)
+    reactor.run()
+    return "Success"
